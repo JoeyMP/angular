ca3c9dcf7703f03595113c7fe9375a37ddb9da74
Joey Melendez <joey.melendez@avantica.net>
2020-02-07 18:06:38 -0500

    heroes service

--- curso/02-spa/src/app/app.module.ts
+++ curso/02-spa/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import {APP_ROUTING} from './app.routes';
 
 //Servicios
-
+import { HeroesService } from './services/heroes.service';
 
 //Components
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +27,9 @@ import { AboutComponent } from './components/about/about.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
