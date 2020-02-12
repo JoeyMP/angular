daf84c231512ba4eaad3a6d4ed4300f772d5222e
Joey Melendez <joey.melendez@avantica.net>
2020-02-11 19:16:10 -0500

    implement component heroe

--- curso/02-spa/src/app/components/heroe/heroe.component.ts
+++ curso/02-spa/src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HeroesService } from "../../services/heroes.service";
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  selector: "app-heroe",
+  templateUrl: "./heroe.component.html",
+  styleUrls: ["./heroe.component.css"]
 })
 export class HeroeComponent implements OnInit {
+  heroe: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService
+  ) {
+    this.activatedRoute.params.subscribe(params => {
+      console.log(params["id"]);
+      this.heroe = heroesService.getHeroe(params["id"]);
+    });
   }
 
+  ngOnInit() {}
 }
