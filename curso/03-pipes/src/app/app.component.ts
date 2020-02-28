7ccb34c3fa63556aba550c0415a844089de27fc0
Joey Melendez <joey.melendez@avantica.net>
2020-02-28 17:48:19 -0500

    pipes-percent

--- curso/03-pipes/src/app/app.component.ts
+++ curso/03-pipes/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  nombre : string = 'Capitán América';
-  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  nombre: string = "Capitán América";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI = Math.PI;
+  porcentaje: number = 0.235;
 }
