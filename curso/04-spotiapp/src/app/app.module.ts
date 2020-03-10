c5253440b759664dc9fc7f145cd6a6aca5d09767
Joey Melendez <joey.melendez@avantica.net>
2020-03-10 18:25:52 -0500

    router, html navigation

--- curso/04-spotiapp/src/app/app.module.ts
+++ curso/04-spotiapp/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/share/navbar/navbar.component';
 import { ArtistaComponent } from './components/artista/artista.component';
+
+//importar rutas
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,8 @@ import { ArtistaComponent } from './components/artista/artista.component';
     ArtistaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
