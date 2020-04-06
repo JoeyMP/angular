d855cfe9fea04550950a06caeb3875fcb9c1cf6e
Joey Melendez <joey.melendez@avantica.net>
2020-04-05 19:12:45 -0500

    ngClass examples

---
+++ curso/06-miscelaneos/src/app/components/clases/clases.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html',
+  styles: []
+})
+export class ClasesComponent implements OnInit {
+
+  alert : string = "alert-danger";
+  
+  myProperties: Object={
+    danger: false
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
