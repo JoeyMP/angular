8909fb59cce30b4773353433f10c938a4865e115
Joey Melendez <joey.melendez@avantica.net>
2020-04-09 09:33:51 -0500

    lifecycle hooks

--- curso/06-miscelaneos/src/app/components/home/home.component.ts
+++ curso/06-miscelaneos/src/app/components/home/home.component.ts
@@ -1,22 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy} from "@angular/core";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   template: `
-   <app-ng-style></app-ng-style>
-    <app-css></app-css>
-    <app-clases></app-clases>
+    <app-ng-style></app-ng-style>
+    <br />
 
-     <p [appResaltado]="'blue'">Hola Mundo</p>
+    <app-css></app-css>
+    <br />
+
+    <app-clases></app-clases>
+    <br /><br />
+
+    <p [appResaltado]="'blue'">Hola Mundo</p>
+    <br />
+
     <app-ng-switch></app-ng-switch>
   `,
-  styles: []
+  styles: [],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnChanges,DoCheck,AfterContentInit,AfterContentChecked,
+    AfterViewInit, AfterViewChecked, OnDestroy {
+  constructor() {
+    console.log("constructor");
+  }
 
   ngOnInit() {
+    console.log("ngOnInit");
+  }
+
+  ngOnChanges(){
+    console.log("ngOnChanges");
+  }
+  ngDoCheck(){
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked");
+  }
+  ngOnDestroy(){
+    console.log("ngOnDestroy");
   }
 
 }
