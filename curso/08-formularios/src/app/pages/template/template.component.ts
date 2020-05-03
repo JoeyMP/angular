161ae3f79c9f09fa728185fb9f6af336ae8595e0
Joey Melendez <joey.melendez@avantica.net>
2020-05-03 12:22:27 -0500

    template: ngModel, validations

--- curso/08-formularios/src/app/pages/template/template.component.ts
+++ curso/08-formularios/src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario={
+    nombre: ''
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  save(forma : NgForm){
+    console.log('submit');
+    console.log(forma);
   }
 
 }
