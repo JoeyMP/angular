bc95031e93a5d6abe43e45437822801e23df38f5
Joey Melendez <joey.melendez@avantica.net>
2020-05-23 12:40:40 -0500

    template: select and radio

--- curso/08-formularios/src/app/pages/template/template.component.ts
+++ curso/08-formularios/src/app/pages/template/template.component.ts
@@ -1,31 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { PaisService } from "src/app/services/pais.service";
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  selector: "app-template",
+  templateUrl: "./template.component.html",
+  styleUrls: ["./template.component.css"],
 })
 export class TemplateComponent implements OnInit {
+  //valores por defecto
+  usuario = {
+    nombre: "Joey",
+    apellido: "Melendez",
+    correo: "joy15.180@gmail.com",
+    pais: ''
+  };
 
-  usuario={
-    nombre: 'Joey',
-    apellido: 'Melendez',
-    correo: 'joy15.180@gmail.com'
-  }
+  paises: any[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
   ngOnInit() {
-  }
-
-  save(forma : NgForm){
-    console.log('submit');
-    if(forma.invalid){
-      Object.values( forma.controls).forEach(control=>{
-        control.markAsTouched();
+    this.paisService.getPaises().subscribe((paises) => {
+      this.paises = paises;
+      this.paises.unshift({
+        nombre: '[Seleccione Pais]',
+        codigo: ''
       })
-    }
+      console.log(this.paises);
+    });
   }
 
+  save(forma: NgForm) {
+    console.log(forma);
+    if (forma.invalid) {
+      //marcar los controles como si hubieran sido tocados
+      Object.values(forma.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+      return;
+    }
+    console.log(forma.value);
+  }
 }
